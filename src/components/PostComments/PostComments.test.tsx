bc99d92b0db8d3ee108cc91577e2c5590d4918b6
LuizSoares0408 <luiz.h.g.soares@outlook.com>
2025-05-27 02:06:29 -0300

    Implementação do teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,24 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from '../PostComments'
 
-describe('Teste para o componente PostComment', () => {
+describe('Teste para o componente PostComments', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComments />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    
+    it('Deve permitir a inserção de dois comentários', () => {
+        render(<PostComments />);
+
+        const commentInput = screen.getByTestId('comment-input');
+        const submitButton = screen.getByTestId('submit-button');
+
+        fireEvent.change(commentInput, { target: { value: 'Meu primeiro comentário!' } });
+        fireEvent.click(submitButton);
+        expect(screen.getByText('Meu primeiro comentário!')).toBeInTheDocument();
+        
+        fireEvent.change(commentInput, { target: { value: 'Meu segundo comentário!' } });
+        fireEvent.click(submitButton);
+        expect(screen.getByText('Meu segundo comentário!')).toBeInTheDocument();
+    })
 });
